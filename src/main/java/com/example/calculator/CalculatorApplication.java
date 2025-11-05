6e9b49bd0ff31259df978fd62267165f7ab42762
Ragul2708 <82565623+Ragul2708@users.noreply.github.com>
2025-11-05 23:46:39 +0530

    Highlight importance of @SpringBootApplication annotation
    Added a comment to emphasize the importance of the @SpringBootApplication annotation.

--- src/main/java/com/example/calculator/CalculatorApplication.java
+++ src/main/java/com/example/calculator/CalculatorApplication.java
@@ -3,8 +3,9 @@ package com.example.calculator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication    // ✅ THIS ANNOTATION IS VERY IMPORTANT
 public class CalculatorApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(CalculatorApplication.class, args);
     }
